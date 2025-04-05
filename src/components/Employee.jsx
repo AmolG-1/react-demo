c4f688270dcc2923e0c311dd562c70edfb8c7fb0
amolG <amolg.react@gmail.com>
2025-04-05 09:17:53 +0530

    employee-heading-change

--- src/components/Employee.jsx
+++ src/components/Employee.jsx
@@ -4,6 +4,7 @@ function Employee(props){
     const {name,city} = props;
     return (
         <>
+            <h1>Employee Details</h1>
             <div className="container">
                 <h1>Employee Name is {name}</h1>
                 <h1>Employee City is {city}</h1>
